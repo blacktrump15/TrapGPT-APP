5f9ee1b788ea30ba3e90ecc3dcc9bb32b499e465
blacktrump15 <blacktrump15@gmail.com>
2025-07-14 10:20:01 -0500

    Create index.js

---
+++ pages/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ color: 'white', background: 'black', padding: '50px' }}>
+      <h1>Welcome to TrapGPT</h1>
+      <p>Street-Smart AI Chat. Login or Subscribe to Start Chatting.</p>
+    </div>
+  );
+}
